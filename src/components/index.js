299f44638675b9fbaaefba2219f17559d4e00174
martinafarah6 <martinafarah6@gmail.com>
2026-09-06 20:08:24 +0300

    Add exports for multiple components in index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Header} from './Header/Header'
+export {default as Hero} from './Hero/Hero'
+export {default as About} from './ِAbout/About'
+export {default as RecipeCard} from './RecipeCard/RecipeCard'
+export {default as RecipeList} from './RecipeList/RecipeList'
+export {default as Footer} from './Footer/Footer'
