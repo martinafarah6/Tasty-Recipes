f8e17498de68aceb1862873b926a270fd33c9303
martinafarah6 <martinafarah6@gmail.com>
2026-09-06 19:35:27 +0300

    Add About component with content and image

---
+++ src/components/About/About.js
@@ -0,0 +1,17 @@
+import './About.css'
+
+function About()
+{
+    return(
+        <section id="about">
+            <div className="about-content">
+                <h2>Tasty Recipes</h2>
+                <p>Tasty Recipes is a simple place for food lovers to discover delicious recipes. Our goal is to make cooking easier and more enjoyable by sharing a variety of tasty meals for everyone</p>
+                <p>Whether you are a beginner or simply looking for new ideas, Tasty Recipes is here to inspire your next meal!</p>
+            </div>
+            <img src="https://i.pinimg.com/736x/4f/7b/13/4f7b13b019d85460dc3e459d693483c7.jpg" alt="about_img" width="200"/>
+        </section>
+    )
+}
+
+export default About;
